7df900db33a710418e4e12603b53bfb20d7d7a97
emanuelaschiopu1-glitch <emanuela.schiopu1@student.usv.ro>
2026-04-27 16:11:58 +0300

    Finalizare - Laborator 7

--- WpfApp1/MainWindow.xaml.cs
+++ WpfApp1/MainWindow.xaml.cs
@@ -1,28 +1,64 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using System.Windows;
-using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
+﻿using System.Windows;
 using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
 
 namespace WpfApp1
 {
-    /// <summary>
-    /// Interaction logic for MainWindow.xaml
-    /// </summary>
     public partial class MainWindow : Window
     {
+        // Cerința 2: Constante
+        private const int MAX_LIMIT = 15;
+        private readonly SolidColorBrush culoareVerde = new SolidColorBrush((Color)ColorConverter.ConvertFromString("#2F5D50"));
+
         public MainWindow()
         {
             InitializeComponent();
+        }
+
+        private void btnAdauga_Click(object sender, RoutedEventArgs e)
+        {
+            if (Valideaza() == 0)
+            {
+                lstEvenimente.Items.Add($"{txtNume.Text} (@{txtLocatie.Text})");
+                MessageBox.Show("Eveniment adăugat!");
+                btnReset_Click(sender, e);
+            }
+        }
+
+        private int Valideaza()
+        {
+            int erori = 0;
+            lblNume.Foreground = culoareVerde;
+            lblLocatie.Foreground = culoareVerde;
+            errNume.Visibility = Visibility.Collapsed;
+            errLocatie.Visibility = Visibility.Collapsed;
+
+            // Validare Nume
+            if (string.IsNullOrWhiteSpace(txtNume.Text) || txtNume.Text.Length > MAX_LIMIT)
+            {
+                lblNume.Foreground = Brushes.Red;
+                errNume.Visibility = Visibility.Visible;
+                erori++;
+            }
+
+            // Validare Locație
+            if (string.IsNullOrWhiteSpace(txtLocatie.Text))
+            {
+                lblLocatie.Foreground = Brushes.Red;
+                errLocatie.Visibility = Visibility.Visible;
+                erori++;
+            }
+
+            return erori;
+        }
+
+        private void btnReset_Click(object sender, RoutedEventArgs e)
+        {
+            txtNume.Clear();
+            txtLocatie.Clear();
+            lblNume.Foreground = culoareVerde;
+            lblLocatie.Foreground = culoareVerde;
+            errNume.Visibility = Visibility.Collapsed;
+            errLocatie.Visibility = Visibility.Collapsed;
         }
     }
 }
